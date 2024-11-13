f0a2cbaeca50fe9a30e25c9bd0fa5b33f1a35228
공태현 <dev0taehyeon@gmail.com>
2024-11-13 20:57:52 +0900

    [feat] : Project Setting #1

---
+++ src/test/java/awtoz/awtoz/AwtozApplicationTests.java
@@ -0,0 +1,13 @@
+package awtoz.awtoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwtozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
