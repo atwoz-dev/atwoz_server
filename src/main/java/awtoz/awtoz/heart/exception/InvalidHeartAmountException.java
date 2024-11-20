34d9aee53dd111f1f9e4293d0132e38adb2236f3
hainho <hainho9704@gmail.com>
2024-11-20 23:33:44 +0900

    feat: 하트 트랜잭션 관련 예외 추가

---
+++ src/main/java/awtoz/awtoz/heart/exception/InvalidHeartAmountException.java
@@ -0,0 +1,7 @@
+package awtoz.awtoz.heart.exception;
+
+public class InvalidHeartAmountException extends RuntimeException {
+    public InvalidHeartAmountException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/awtoz/awtoz/heart/exception/InvalidHeartTransactionTypeException.java
@@ -0,0 +1,7 @@
+package awtoz.awtoz.heart.exception;
+
+public class InvalidHeartTransactionTypeException extends RuntimeException {
+    public InvalidHeartTransactionTypeException(String message) {
+        super(message);
+    }
+}
