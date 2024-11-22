737ae716830ce8d679136b2dc0f34351191bffac
Hoyun Jung <jhy9094@gmail.com>
2024-11-22 17:48:04 +0900

    test: EmailTest 작성
    #10

--- src/main/java/atwoz/atwoz/common/domain/Email.java
+++ src/main/java/atwoz/atwoz/common/domain/Email.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.regex.Pattern;
@@ -16,6 +17,7 @@ public class Email {
     private static final String EMAIL_REGEX = "^[\\w.%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
+    @Getter
     @Column(name = "email")
     private final String address;
 
