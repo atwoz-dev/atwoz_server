1150783eccf562a59c9fc037c5192eb2be36925d
hainho <hainho9704@gmail.com>
2024-11-27 04:23:04 +0900

    refactor: HeartAmount 값 객체 final 적용

--- src/main/java/awtoz/awtoz/heart/domain/vo/HeartAmount.java
+++ src/main/java/awtoz/awtoz/heart/domain/vo/HeartAmount.java
@@ -2,37 +2,47 @@ package awtoz.awtoz.heart.domain.vo;
 
 import awtoz.awtoz.heart.exception.InvalidHeartAmountException;
 import jakarta.persistence.Embeddable;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Embeddable
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class HeartAmount {
+public final class HeartAmount {
+    private static final Long MAX_USING_AMOUNT = 0L;
+    private static final Long MIN_GAINING_AMOUNT = 1L;
     @Getter
-    private Long amount;
+    private final Long amount;
 
-    public static HeartAmount use(Long amount) {
-        validateAmountIsZeroOrLess(amount);
+    protected HeartAmount() {
+        this.amount = 0L;
+    }
+
+    public static HeartAmount createUsedAmount(Long amount) {
+        validateUsingAmount(amount);
         return new HeartAmount(amount);
     }
 
-    public static HeartAmount gain(Long amount) {
-        validateAmountIsGreaterThanZero(amount);
+    public static HeartAmount createGainedAmount(Long amount) {
+        validateGainingAmount(amount);
         return new HeartAmount(amount);
     }
 
-    private static void validateAmountIsZeroOrLess(Long amount) {
-        if (amount > 0) {
-            throw new InvalidHeartAmountException("사용량은 0 이하의 값이어야 합니다. amount: " + amount);
+    public boolean isUsingAmount() {
+        return this.amount <= MAX_USING_AMOUNT;
+    }
+
+    public boolean isGainingAmount() {
+        return this.amount >= MIN_GAINING_AMOUNT;
+    }
+
+    private static void validateUsingAmount(Long amount) {
+        if (amount > MAX_USING_AMOUNT) {
+            throw new InvalidHeartAmountException("잘못된 하트 사용량 입니다. amount: " + amount);
         }
     }
 
-    private static void validateAmountIsGreaterThanZero(Long amount) {
-        if (amount <= 0) {
-            throw new InvalidHeartAmountException("얻는 값은 1 이상의 값이어야 합니다. amount: " + amount);
+    private static void validateGainingAmount(Long amount) {
+        if (amount < MIN_GAINING_AMOUNT) {
+            throw new InvalidHeartAmountException("잘못된 하트 획득량 입니다. amount: " + amount);
         }
     }
 }
