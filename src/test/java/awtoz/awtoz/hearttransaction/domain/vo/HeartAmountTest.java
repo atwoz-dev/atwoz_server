2ef464cff2e9a9445f6118b72f037e66957af5c1
hainho <hainho9704@gmail.com>
2024-11-28 01:41:13 +0900

    test: HeartAmount 변경 사항에 따라 테스트 수정

--- src/test/java/awtoz/awtoz/hearttransaction/domain/vo/HeartAmountTest.java
+++ src/test/java/awtoz/awtoz/hearttransaction/domain/vo/HeartAmountTest.java
@@ -1,88 +1,131 @@
 package awtoz.awtoz.hearttransaction.domain.vo;
 
-import awtoz.awtoz.hearttransaction.exception.InvalidHeartAmountException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class HeartAmountTest {
 
     @Nested
-    @DisplayName("use 메서드 테스트")
-    class UseTest {
+    @DisplayName("from 메서드 테스트")
+    class FromTest {
         @Test
-        @DisplayName("amount값 0으로 하트 사용하는 HeartAmount 생성 성공")
-        void useTestWithZeroValue() {
+        @DisplayName("amount값 0으로 from 메서드 성공")
+        void fromTestWithZeroValue() {
             // given
             Long amount = 0L;
             // when
-            HeartAmount heartAmount = HeartAmount.use(amount);
+            HeartAmount heartAmount = HeartAmount.from(amount);
             // then
             assertThat(heartAmount.getAmount()).isEqualTo(amount);
         }
 
         @Test
-        @DisplayName("amount값 음수로 하트 사용하는 HeartAmount 생성 성공")
-        void useTestWithNegativeValue() {
+        @DisplayName("amount값 음수로 from 메서드 성공")
+        void fromTestWithNegativeValue() {
             // given
             Long amount = -1L;
             // when
-            HeartAmount heartAmount = HeartAmount.use(amount);
+            HeartAmount heartAmount = HeartAmount.from(amount);
             // then
             assertThat(heartAmount.getAmount()).isEqualTo(amount);
         }
 
         @Test
-        @DisplayName("amount값 양수로 하트 사용하는 HeartAmount 생성시 예외발생")
-        void useTestWithPositiveValue() {
+        @DisplayName("amount값 양수로 from 메서드 성공")
+        void fromTestWithPositiveValue() {
             // given
             Long amount = 1L;
             // when
+            HeartAmount heartAmount = HeartAmount.from(amount);
             // then
-            assertThatThrownBy(() -> HeartAmount.use(amount))
-                    .isInstanceOf(InvalidHeartAmountException.class);
+            assertThat(heartAmount.getAmount()).isEqualTo(amount);
         }
     }
 
     @Nested
-    @DisplayName("gain 메서드 테스트")
-    class GainTest {
+    @DisplayName("isUsingAmount 메서드 테스트")
+    class isUsingAmountTest {
 
         @Test
-        @DisplayName("amount값 양수로 하트 얻는 HeartAmount 생성 성공")
-        void gainTestWithPositiveValue() {
+        @DisplayName("amount값 양수로 isUsingAmount 메서드 False 리턴")
+        void isUsingAmountTestWithPositiveValue() {
             // given
             Long amount = 1L;
+            HeartAmount heartAmount = HeartAmount.from(amount);
             // when
-            HeartAmount heartAmount = HeartAmount.gain(amount);
+            boolean result = heartAmount.isUsingAmount();
             // then
-            assertThat(heartAmount.getAmount()).isEqualTo(amount);
+            assertThat(result).isFalse();
         }
 
         @Test
-        @DisplayName("amount값 0으로 하트 얻는 HeartAmount 생성시 예외발생")
-        void gainTestWithZeroValue() {
+        @DisplayName("amount값 0으로 isUsingAmount 메서드 True 리턴")
+        void isUsingAmountTestWithZeroValue() {
             // given
             Long amount = 0L;
+            HeartAmount heartAmount = HeartAmount.from(amount);
             // when
+            boolean result = heartAmount.isUsingAmount();
             // then
-            assertThatThrownBy(() -> HeartAmount.gain(amount))
-                    .isInstanceOf(InvalidHeartAmountException.class);
+            assertThat(result).isTrue();
         }
 
         @Test
-        @DisplayName("amount값 음수로 하트 얻는 HeartAmount 생성시 예외발생")
-        void gainTestWithNegativeValue() {
+        @DisplayName("amount값 음수로 isUsingAmount 메서드 True 리턴")
+        void isUsingAmountTestWithNegativeValue() {
             // given
             Long amount = -1L;
+            HeartAmount heartAmount = HeartAmount.from(amount);
             // when
+            boolean result = heartAmount.isUsingAmount();
             // then
-            assertThatThrownBy(() -> HeartAmount.gain(amount))
-                    .isInstanceOf(InvalidHeartAmountException.class);
+            assertThat(result).isTrue();
         }
+    }
+
+    @Nested
+    @DisplayName("isGainingAmount 메서드 테스트")
+    class isGainingAmountTest {
+
+        @Test
+        @DisplayName("amount값 양수로 isGainingAmount 메서드 True 리턴")
+        void isGainingAmountTestWithPositiveValue() {
+            // given
+            Long amount = 1L;
+            HeartAmount heartAmount = HeartAmount.from(amount);
+            // when
+            boolean result = heartAmount.isGainingAmount();
+            // then
+            assertThat(result).isTrue();
+        }
+
+        @Test
+        @DisplayName("amount값 0으로 isGainingAmount 메서드 False 리턴")
+        void isGainingAmountTestWithZeroValue() {
+            // given
+            Long amount = 0L;
+            HeartAmount heartAmount = HeartAmount.from(amount);
+            // when
+            boolean result = heartAmount.isGainingAmount();
+            // then
+            assertThat(result).isFalse();
+        }
+
+        @Test
+        @DisplayName("amount값 음수로 isGainingAmount 메서드 False 리턴")
+        void isGainingAmountTestWithNegativeValue() {
+            // given
+            Long amount = -1L;
+            HeartAmount heartAmount = HeartAmount.from(amount);
+            // when
+            boolean result = heartAmount.isGainingAmount();
+            // then
+            assertThat(result).isFalse();
+        }
+
     }
 }
