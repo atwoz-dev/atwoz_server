0845fecd9b12a579e3536aad1989e988707cd2f4
hainho <hainho9704@gmail.com>
2024-11-29 11:52:41 +0900

    feat: SoftDeleteBaseEntity에 @SQLRestriction 제거

--- src/main/java/atwoz/atwoz/common/domain/SoftDeleteBaseEntity.java
+++ src/main/java/atwoz/atwoz/common/domain/SoftDeleteBaseEntity.java
@@ -2,16 +2,25 @@ package atwoz.atwoz.common.domain;
 
 
 import jakarta.persistence.MappedSuperclass;
-import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@SQLRestriction("deleted_at IS NULL")
 public abstract class SoftDeleteBaseEntity extends BaseEntity {
     private LocalDateTime deletedAt;
 
-    public void delete() {
+    public void softDelete() {
+        validateSoftDelete();
         this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+
+    private void validateSoftDelete() {
+        if (isDeleted()) {
+            throw new IllegalStateException("이미 삭제된 엔티티 입니다.");
+        }
     }
 }
