344b1c34b9c456f3cf29d02edb41e84fb11b61c7
hainho <hainho9704@gmail.com>
2024-11-30 17:08:22 +0900

    feat: softDelete 메서드 삭제

--- src/main/java/atwoz/atwoz/common/domain/SoftDeleteBaseEntity.java
+++ src/main/java/atwoz/atwoz/common/domain/SoftDeleteBaseEntity.java
@@ -9,18 +9,7 @@ import java.time.LocalDateTime;
 public abstract class SoftDeleteBaseEntity extends BaseEntity {
     private LocalDateTime deletedAt;
 
-    public void softDelete() {
-        validateSoftDelete();
-        this.deletedAt = LocalDateTime.now();
-    }
-
     public boolean isDeleted() {
         return this.deletedAt != null;
-    }
-
-    private void validateSoftDelete() {
-        if (isDeleted()) {
-            throw new IllegalStateException("이미 삭제된 엔티티 입니다.");
-        }
     }
 }
