a9a4eb8c87c9ffdb698d1516d75a9d47890d56ee
hainho <hainho9704@gmail.com>
2024-12-02 17:34:51 +0900

    test: HeartPriceAmountTest 작성

---
+++ src/test/java/atwoz/atwoz/heartusagepolicy/domain/HeartPriceAmountTest.java
@@ -0,0 +1,33 @@
+package atwoz.atwoz.heartusagepolicy.domain;
+
+
+import atwoz.atwoz.heartusagepolicy.exception.InvalidHeartPriceAmountException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class HeartPriceAmountTest {
+
+    @Test
+    @DisplayName("price 값이 0이면 InvalidHeartPriceAmountException 발생")
+    void fromTestWhenPriceIsZero() {
+        // given
+        Long price = 0L;
+        // when & then
+        assertThatThrownBy(() -> HeartPriceAmount.from(price))
+                .isInstanceOf(InvalidHeartPriceAmountException.class);
+    }
+
+    @Test
+    @DisplayName("price 값이 1 이상이면 InvalidHeartPriceAmountException 발생")
+    void fromTestWhenPriceIsPositive() {
+        // given
+        Long price = 1L;
+        // when
+        HeartPriceAmount heartPriceAmount = HeartPriceAmount.from(price);
+        // then
+        assertThat(heartPriceAmount).isNotNull();
+    }
+}
