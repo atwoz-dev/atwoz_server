d4832236fb732bfce304430a4d4c0dac6adfe9aa
hainho <hainho9704@gmail.com>
2024-12-02 17:56:23 +0900

    test: UsageTypeMapperTest 에서 UsageType이 추가되고 Mapper에는 반영되지 않은 경우를 확인할 수 있도록 테스트 수정

--- src/test/java/atwoz/atwoz/heartusagepolicy/domain/UsageTypeMapperTest.java
+++ src/test/java/atwoz/atwoz/heartusagepolicy/domain/UsageTypeMapperTest.java
@@ -20,46 +20,14 @@ class UsageTypeMapperTest {
     }
 
     @Test
-    @DisplayName("UsageType이 MESSAGE이면 TransactionType.MESSAGE 반환")
-    void toTransactionTypeTestWhenUsageTypeIsMessage() {
+    @DisplayName("UsageType의 모든 값이 Mapper에 정의되어 있는지 테스트")
+    void toTransactionTypeTestWithAllOfUsageType() {
         // given
-        UsageType usageType = UsageType.MESSAGE;
-        // when
-        TransactionType transactionType = UsageTypeMapper.toTransactionType(usageType);
-        // then
-        assertThat(transactionType).isEqualTo(TransactionType.MESSAGE);
-    }
-
-    @Test
-    @DisplayName("UsageType이 DATING_EXAM이면 TransactionType.DATING_EXAM 반환")
-    void toTransactionTypeTestWhenUsageTypeIsDatingExam() {
-        // given
-        UsageType usageType = UsageType.DATING_EXAM;
-        // when
-        TransactionType transactionType = UsageTypeMapper.toTransactionType(usageType);
-        // then
-        assertThat(transactionType).isEqualTo(TransactionType.DATING_EXAM);
-    }
-
-    @Test
-    @DisplayName("UsageType이 PROFILE_EXCHANGE이면 TransactionType.PROFILE_EXCHANGE 반환")
-    void toTransactionTypeTestWhenUsageTypeIsProfileExchange() {
-        // given
-        UsageType usageType = UsageType.PROFILE_EXCHANGE;
-        // when
-        TransactionType transactionType = UsageTypeMapper.toTransactionType(usageType);
-        // then
-        assertThat(transactionType).isEqualTo(TransactionType.PROFILE_EXCHANGE);
-    }
-
-    @Test
-    @DisplayName("UsageType이 SELF_INTRODUCTION이면 TransactionType.SELF_INTRODUCTION 반환")
-    void toTransactionTypeTestWhenUsageTypeIsSelfIntroduction() {
-        // given
-        UsageType usageType = UsageType.SELF_INTRODUCTION;
-        // when
-        TransactionType transactionType = UsageTypeMapper.toTransactionType(usageType);
-        // then
-        assertThat(transactionType).isEqualTo(TransactionType.SELF_INTRODUCTION);
+        UsageType[] allValues = UsageType.values();
+        // when & then
+        for (UsageType usageType : allValues) {
+            TransactionType transactionType = UsageTypeMapper.toTransactionType(usageType);
+            assertThat(transactionType).isNotNull();
+        }
     }
 }
