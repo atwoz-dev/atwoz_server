1cea7fedb9a258adb9da93e17371c869ff8399f9
hainho <hainho9704@gmail.com>
2024-12-03 07:39:43 +0900

    refactor: UsageTypeException 삭제

--- src/main/java/atwoz/atwoz/heartusagepolicy/exception/InvalidUsageTypeException.java
+++
@@ -1,7 +0,0 @@
-package atwoz.atwoz.heartusagepolicy.exception;
-
-public class InvalidUsageTypeException extends RuntimeException {
-    public InvalidUsageTypeException(String message) {
-        super(message);
-    }
-}
