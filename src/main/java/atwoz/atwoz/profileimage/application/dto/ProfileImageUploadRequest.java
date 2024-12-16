c208c79dd85eee02e018d6c69638b43d07ede0bd
공태현 <dev0taehyeon@gmail.com>
2024-12-16 20:50:48 +0900

    fix : 테스트 코드를 위한 생성자 어노테이션 추가

--- src/main/java/atwoz/atwoz/profileimage/application/dto/ProfileImageUploadRequest.java
+++ src/main/java/atwoz/atwoz/profileimage/application/dto/ProfileImageUploadRequest.java
@@ -1,11 +1,13 @@
 package atwoz.atwoz.profileimage.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ProfileImageUploadRequest {
     MultipartFile image;
     Boolean isPrimary;
