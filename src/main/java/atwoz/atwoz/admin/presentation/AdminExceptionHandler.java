6da5c4acea10efaf5ba14ce76a6d9fc36f9e2800
hoyunjung <jhy9094@gmail.com>
2024-12-19 22:07:13 +0900

    feat: AdminExceptionHandler 정의
    #26

--- src/main/java/atwoz/atwoz/admin/presentation/AdminExceptionHandler.java
+++ src/main/java/atwoz/atwoz/admin/presentation/AdminExceptionHandler.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(basePackageClasses = AdminAuthController.class)
 public class AdminExceptionHandler {
 
     @ExceptionHandler(DuplicateEmailException.class)
     public ResponseEntity<BaseResponse<Void>> handleDuplicateEmailException(DuplicateEmailException e) {
-        log.error(e.getMessage());
+        log.error("관리자에 회원가입에 실패했습니다. {}", e.getMessage(), e);
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(BaseResponse.from(StatusType.INVALID_DUPLICATE_VALUE));
