f496ce1c8d418023aab9dd09c8116badf6181231
hainho <hainho9704@gmail.com>
2024-12-20 16:46:21 +0900

    fix: 불필요한 재정의 삭제

--- src/main/java/atwoz/atwoz/hearttransaction/infra/HeartTransactionJpaRepository.java
+++ src/main/java/atwoz/atwoz/hearttransaction/infra/HeartTransactionJpaRepository.java
@@ -4,5 +4,4 @@ import atwoz.atwoz.hearttransaction.domain.HeartTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HeartTransactionJpaRepository extends JpaRepository<HeartTransaction, Long> {
-    HeartTransaction save(HeartTransaction heartTransaction);
 }
