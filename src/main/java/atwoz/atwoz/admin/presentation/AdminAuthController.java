32d74238f9a54bc938a378e107f1ddec37595efe
Hoyun Jung <jhy9094@gmail.com>
2024-12-26 13:52:24 +0900

    feat: 관리자 로그인 컨트롤러 구현
    #35

--- src/main/java/atwoz/atwoz/admin/presentation/AdminAuthController.java
+++ src/main/java/atwoz/atwoz/admin/presentation/AdminAuthController.java
@@ -1,12 +1,16 @@
 package atwoz.atwoz.admin.presentation;
 
 import atwoz.atwoz.admin.application.AdminAuthService;
+import atwoz.atwoz.admin.application.dto.AdminLoginRequest;
+import atwoz.atwoz.admin.application.dto.AdminLoginResponse;
 import atwoz.atwoz.admin.application.dto.AdminSignupRequest;
 import atwoz.atwoz.admin.application.dto.AdminSignupResponse;
 import atwoz.atwoz.common.presentation.BaseResponse;
 import atwoz.atwoz.common.presentation.StatusType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,8 +25,28 @@ public class AdminAuthController {
     private final AdminAuthService adminAuthService;
 
     @PostMapping("/signup")
-    public ResponseEntity<BaseResponse<AdminSignupResponse>> signUp(@Valid @RequestBody AdminSignupRequest request) {
+    public ResponseEntity<BaseResponse<AdminSignupResponse>> signup(@Valid @RequestBody AdminSignupRequest request) {
         AdminSignupResponse data = adminAuthService.signup(request);
         return ResponseEntity.ok(BaseResponse.of(StatusType.OK, data));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<BaseResponse<Void>> login(@Valid @RequestBody AdminLoginRequest request) {
+        AdminLoginResponse response = adminAuthService.login(request);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + response.accessToken());
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", response.refreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/")
+                .maxAge(60 * 60 * 24 * 7 * 4)
+                .build();
+        headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(BaseResponse.from(StatusType.OK));
     }
 }
