540804f27549d35139d27abeb008b9bee8364187
공태현 <dev0taehyeon@gmail.com>
2025-01-09 14:05:20 +0900

    fix : 사용하지 않는 주석 제거.

--- src/main/java/atwoz/atwoz/AtwozApplication.java
+++ src/main/java/atwoz/atwoz/AtwozApplication.java
@@ -1,13 +1,11 @@
 package atwoz.atwoz;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@Slf4j
 public class AtwozApplication {
     public static void main(String[] args) {
         SpringApplication.run(AtwozApplication.class, args);
