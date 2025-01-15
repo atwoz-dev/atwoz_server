626ef7fd0e18aab44f0064a7cbcd1fe3c7883d8c
공태현 <dev0taehyeon@gmail.com>
2025-01-15 17:12:32 +0900

    refact : Swagger Local/Prod 모두 추가.

--- src/main/java/atwoz/atwoz/common/config/SwaggerConfig.java
+++ src/main/java/atwoz/atwoz/common/config/SwaggerConfig.java
@@ -5,8 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -24,6 +28,12 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Atwoz API")
                         .version("1.0")
-                        .description("Atwoz API Specification"));
+                        .description("Atwoz API Specification"))
+
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local environment"),
+                        new Server().url("https://api.atwoz.kr").description("Production environment")
+                ));
+
     }
 }
