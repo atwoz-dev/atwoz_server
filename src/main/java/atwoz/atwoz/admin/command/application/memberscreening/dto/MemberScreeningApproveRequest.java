24ce1bb2d7a28f14fdf1b6543c3970d1f9fc338c
hoyunjung <jhy9094@gmail.com>
2025-02-05 12:03:19 +0900

    refactor: 불필요한 version 필드 삭제
    #56

--- src/main/java/atwoz/atwoz/admin/command/application/memberscreening/dto/MemberScreeningApproveRequest.java
+++ src/main/java/atwoz/atwoz/admin/command/application/memberscreening/dto/MemberScreeningApproveRequest.java
@@ -4,9 +4,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record MemberScreeningApproveRequest(
         @NotNull(message = "멤버 id는 null일 수 없습니다.")
-        Long memberId,
-
-        @NotNull(message = "version은 null일 수 없습니다.")
-        Long version
+        Long memberId
 ) {
 }
--- src/main/java/atwoz/atwoz/admin/command/application/memberscreening/dto/MemberScreeningRejectRequest.java
+++ src/main/java/atwoz/atwoz/admin/command/application/memberscreening/dto/MemberScreeningRejectRequest.java
@@ -8,9 +8,6 @@ public record MemberScreeningRejectRequest(
         Long memberId,
 
         @NotBlank(message = "반려 사유를 선택해주세요.")
-        String rejectionReason,
-
-        @NotNull(message = "version은 null일 수 없습니다.")
-        Long version
+        String rejectionReason
 ) {
 }
