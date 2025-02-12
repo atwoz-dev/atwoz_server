4af385af86b2e0ee3f9fa2a3c34b6597a6759528
hainho <hainho9704@gmail.com>
2025-02-12 18:50:15 +0900

    feat: 최초 인터뷰 제출 이벤트 클래스 파일 경로 수정

--- src/main/java/atwoz/atwoz/interview/command/application/answer/FirstInterviewSubmittedEvent.java
+++ src/main/java/atwoz/atwoz/interview/command/application/answer/FirstInterviewSubmittedEvent.java
@@ -1,11 +1,10 @@
-package atwoz.atwoz.admin.command.application.memberscreening;
+package atwoz.atwoz.interview.command.application.answer;
 
 import atwoz.atwoz.common.event.Event;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-// TODO: 인터뷰 쪽 개발되면 그쪽으로 옮겨야함
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class FirstInterviewSubmittedEvent extends Event {
