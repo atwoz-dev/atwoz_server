2824dd132994e6163d8b4c043f4a5be5c60087ac
hainho <hainho9704@gmail.com>
2025-02-17 23:09:07 +0900

    test: 이미 답변이 존재하는 경우 예외 처리 추가

--- src/test/java/atwoz/atwoz/interview/command/application/answer/InterviewAnswerServiceTest.java
+++ src/test/java/atwoz/atwoz/interview/command/application/answer/InterviewAnswerServiceTest.java
@@ -1,5 +1,6 @@
 package atwoz.atwoz.interview.command.application.answer;
 
+import atwoz.atwoz.interview.command.application.answer.exception.InterviewAnserAlreadyExistsException;
 import atwoz.atwoz.interview.command.application.answer.exception.InterviewQuestionIsNotPublicException;
 import atwoz.atwoz.interview.command.application.answer.exception.InterviewQuestionNotFoundException;
 import atwoz.atwoz.interview.command.domain.answer.InterviewAnswer;
@@ -66,6 +67,25 @@ class InterviewAnswerServiceTest {
     }
 
     @Test
+    @DisplayName("인터뷰 답변이 이미 존재한다면 예외를 던진다.")
+    void throwsExceptionWhenInterviewAnswerAlreadyExists() {
+        // given
+        Long interviewQuestionId = 1L;
+        String answerContent = "content";
+        InterviewAnswerSaveRequest request = new InterviewAnswerSaveRequest(interviewQuestionId, answerContent);
+        Long memberId = 2L;
+
+        InterviewQuestion interviewQuestion = mock(InterviewQuestion.class);
+        when(interviewQuestion.isPublic()).thenReturn(true);
+        when(interviewQuestionCommandRepository.findById(interviewQuestionId)).thenReturn(Optional.of(interviewQuestion));
+        when(interviewAnswerCommandRepository.existsByQuestionIdAndMemberId(interviewQuestionId, memberId)).thenReturn(true);
+
+        // when & then
+        assertThatThrownBy(() -> interviewAnswerService.saveAnswer(request, memberId))
+                .isInstanceOf(InterviewAnserAlreadyExistsException.class);
+    }
+
+    @Test
     @DisplayName("다른 인터뷰 답변이 존재하지 않으면 submitFirstInterviewAnswer를 호출한다.")
     void callsSubmitFirstInterviewAnswerWhenOtherInterviewAnswerDoesNotExist() {
         // given
@@ -78,6 +98,7 @@ class InterviewAnswerServiceTest {
         when(interviewQuestion.isPublic()).thenReturn(true);
         when(interviewQuestionCommandRepository.findById(interviewQuestionId)).thenReturn(Optional.of(interviewQuestion));
         when(interviewAnswerCommandRepository.existsByMemberId(memberId)).thenReturn(false);
+        when(interviewAnswerCommandRepository.existsByQuestionIdAndMemberId(interviewQuestionId, memberId)).thenReturn(false);
 
         InterviewAnswer interviewAnswer = mock(InterviewAnswer.class);
         try (MockedStatic<InterviewAnswer> mockedStatic = mockStatic(InterviewAnswer.class)) {
@@ -107,6 +128,7 @@ class InterviewAnswerServiceTest {
         when(interviewQuestion.isPublic()).thenReturn(true);
         when(interviewQuestionCommandRepository.findById(interviewQuestionId)).thenReturn(Optional.of(interviewQuestion));
         when(interviewAnswerCommandRepository.existsByMemberId(memberId)).thenReturn(true);
+        when(interviewAnswerCommandRepository.existsByQuestionIdAndMemberId(interviewQuestionId, memberId)).thenReturn(false);
 
         InterviewAnswer interviewAnswer = mock(InterviewAnswer.class);
         try (MockedStatic<InterviewAnswer> mockedStatic = mockStatic(InterviewAnswer.class)) {
