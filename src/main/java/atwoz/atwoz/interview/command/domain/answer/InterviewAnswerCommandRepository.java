7347a199a4030f36cedd9482c3355a0fa426a066
hainho <hainho9704@gmail.com>
2025-02-17 22:32:03 +0900

    fix: 사용하지 않는 메서드 삭제

--- src/main/java/atwoz/atwoz/interview/command/domain/answer/InterviewAnswerCommandRepository.java
+++ src/main/java/atwoz/atwoz/interview/command/domain/answer/InterviewAnswerCommandRepository.java
@@ -3,6 +3,5 @@ package atwoz.atwoz.interview.command.domain.answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterviewAnswerCommandRepository extends JpaRepository<InterviewAnswer, Long> {
-    boolean existsByQuestionIdAndMemberId(Long questionId, Long memberId);
     boolean existsByMemberId(Long memberId);
 }
