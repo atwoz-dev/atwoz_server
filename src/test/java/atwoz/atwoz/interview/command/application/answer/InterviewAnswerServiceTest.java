aaf9b9af1a0954e4147a8aefbfe88d740dd5a93c
hainho <hainho9704@gmail.com>
2025-02-17 23:00:29 +0900

    test: InterviewQuestion이 공개되지 않은 경우 예외 처리 추가

--- src/test/java/atwoz/atwoz/interview/command/application/answer/InterviewAnswerServiceTest.java
+++ src/test/java/atwoz/atwoz/interview/command/application/answer/InterviewAnswerServiceTest.java
@@ -1,8 +1,10 @@
 package atwoz.atwoz.interview.command.application.answer;
 
+import atwoz.atwoz.interview.command.application.answer.exception.InterviewQuestionIsNotPublicException;
 import atwoz.atwoz.interview.command.application.answer.exception.InterviewQuestionNotFoundException;
 import atwoz.atwoz.interview.command.domain.answer.InterviewAnswer;
 import atwoz.atwoz.interview.command.domain.answer.InterviewAnswerCommandRepository;
+import atwoz.atwoz.interview.command.domain.question.InterviewQuestion;
 import atwoz.atwoz.interview.command.domain.question.InterviewQuestionCommandRepository;
 import atwoz.atwoz.interview.presentation.answer.dto.InterviewAnswerSaveRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
@@ -36,11 +40,29 @@ class InterviewAnswerServiceTest {
         InterviewAnswerSaveRequest request = new InterviewAnswerSaveRequest(interviewQuestionId, answerContent);
         Long memberId = 2L;
 
-        when(interviewQuestionCommandRepository.existsById(interviewQuestionId)).thenReturn(false);
+        when(interviewQuestionCommandRepository.findById(interviewQuestionId)).thenReturn(Optional.empty());
 
         // when & then
         assertThatThrownBy(() -> interviewAnswerService.saveAnswer(request, memberId))
                 .isInstanceOf(InterviewQuestionNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("인터뷰 질문이 공개되어 있지 않다면 예외를 던진다.")
+    void throwsExceptionWhenInterviewQuestionIsNotPublic() {
+        // given
+        Long interviewQuestionId = 1L;
+        String answerContent = "content";
+        InterviewAnswerSaveRequest request = new InterviewAnswerSaveRequest(interviewQuestionId, answerContent);
+        Long memberId = 2L;
+
+        InterviewQuestion interviewQuestion = mock(InterviewQuestion.class);
+        when(interviewQuestion.isPublic()).thenReturn(false);
+        when(interviewQuestionCommandRepository.findById(interviewQuestionId)).thenReturn(Optional.of(interviewQuestion));
+
+        // when & then
+        assertThatThrownBy(() -> interviewAnswerService.saveAnswer(request, memberId))
+                .isInstanceOf(InterviewQuestionIsNotPublicException.class);
     }
 
     @Test
@@ -52,7 +74,9 @@ class InterviewAnswerServiceTest {
         InterviewAnswerSaveRequest request = new InterviewAnswerSaveRequest(interviewQuestionId, answerContent);
         Long memberId = 2L;
 
-        when(interviewQuestionCommandRepository.existsById(interviewQuestionId)).thenReturn(true);
+        InterviewQuestion interviewQuestion = mock(InterviewQuestion.class);
+        when(interviewQuestion.isPublic()).thenReturn(true);
+        when(interviewQuestionCommandRepository.findById(interviewQuestionId)).thenReturn(Optional.of(interviewQuestion));
         when(interviewAnswerCommandRepository.existsByMemberId(memberId)).thenReturn(false);
 
         InterviewAnswer interviewAnswer = mock(InterviewAnswer.class);
@@ -79,7 +103,9 @@ class InterviewAnswerServiceTest {
         InterviewAnswerSaveRequest request = new InterviewAnswerSaveRequest(interviewQuestionId, answerContent);
         Long memberId = 2L;
 
-        when(interviewQuestionCommandRepository.existsById(interviewQuestionId)).thenReturn(true);
+        InterviewQuestion interviewQuestion = mock(InterviewQuestion.class);
+        when(interviewQuestion.isPublic()).thenReturn(true);
+        when(interviewQuestionCommandRepository.findById(interviewQuestionId)).thenReturn(Optional.of(interviewQuestion));
         when(interviewAnswerCommandRepository.existsByMemberId(memberId)).thenReturn(true);
 
         InterviewAnswer interviewAnswer = mock(InterviewAnswer.class);
