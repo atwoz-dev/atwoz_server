46631062d252df69dfd567b3a2a27098c69514bd
hainho <hainho9704@gmail.com>
2025-02-17 21:46:37 +0900

    feat: InterviewAnswerController 구현

---
+++ src/main/java/atwoz/atwoz/interview/presentation/answer/InterviewAnswerController.java
@@ -0,0 +1,24 @@
+package atwoz.atwoz.interview.presentation.answer;
+
+import atwoz.atwoz.auth.presentation.AuthContext;
+import atwoz.atwoz.auth.presentation.AuthPrincipal;
+import atwoz.atwoz.interview.command.application.answer.InterviewAnswerService;
+import atwoz.atwoz.interview.presentation.answer.dto.InterviewAnswerSaveRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/interview/answer")
+@RequiredArgsConstructor
+public class InterviewAnswerController {
+
+        private final InterviewAnswerService interviewAnswerService;
+
+         @PostMapping
+         public void saveAnswer(@RequestBody InterviewAnswerSaveRequest request, @AuthPrincipal AuthContext authContext) {
+             interviewAnswerService.saveAnswer(request, authContext.getId());
+         }
+}
