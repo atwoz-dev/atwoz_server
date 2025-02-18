f95cefaab478464634e95bce488c9d5b19a23683
hainho <hainho9704@gmail.com>
2025-02-18 20:24:16 +0900

    feat: 인터뷰 질문 업데이트 컨트롤러 구현

--- src/main/java/atwoz/atwoz/interview/presentation/question/InterviewQuestionController.java
+++ src/main/java/atwoz/atwoz/interview/presentation/question/InterviewQuestionController.java
@@ -4,10 +4,10 @@ import atwoz.atwoz.common.enums.StatusType;
 import atwoz.atwoz.common.response.BaseResponse;
 import atwoz.atwoz.interview.command.application.question.InterviewQuestionService;
 import atwoz.atwoz.interview.presentation.question.dto.InterviewQuestionSaveRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +15,15 @@ public class InterviewQuestionController {
 
     private final InterviewQuestionService interviewQuestionService;
 
-    @PostMapping("/admin/interview/questions")
-    public ResponseEntity<BaseResponse<Void>> saveQuestion(InterviewQuestionSaveRequest request) {
+    @PostMapping("/admin/interview/question")
+    public ResponseEntity<BaseResponse<Void>> saveQuestion(@Valid @RequestBody InterviewQuestionSaveRequest request) {
         interviewQuestionService.saveQuestion(request);
+        return ResponseEntity.ok(BaseResponse.from(StatusType.OK));
+    }
+
+    @PatchMapping("/admin/interview/question/{id}")
+    public ResponseEntity<BaseResponse<Void>> updateQuestion(@PathVariable Long id, @Valid @RequestBody InterviewQuestionSaveRequest request) {
+        interviewQuestionService.updateQuestion(id, request);
         return ResponseEntity.ok(BaseResponse.from(StatusType.OK));
     }
 }
