f97e4afc5a535fa7327da31918b6e9b2922c7259
hainho <hainho9704@gmail.com>
2025-02-18 20:38:10 +0900

    test: update 메서드 테스트

--- src/test/java/atwoz/atwoz/interview/command/domain/question/InterviewQuestionTest.java
+++ src/test/java/atwoz/atwoz/interview/command/domain/question/InterviewQuestionTest.java
@@ -2,6 +2,7 @@ package atwoz.atwoz.interview.command.domain.question;
 
 import atwoz.atwoz.interview.command.domain.question.exception.InvalidInterviewQuestionContentException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,45 +12,101 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InterviewQuestionTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"content is null", "interviewCategory is null"})
-    @DisplayName("of 메서드에서 필드 값이 null이면 예외를 던집니다.")
-    void throwsExceptionWhenFieldValueIsNull(String fieldName) {
-        // given
-        String content = fieldName.equals("content is null") ? null : "content";
-        InterviewCategory interviewCategory = fieldName.equals("interviewCategory is null") ? null : InterviewCategory.PERSONAL;
-        boolean isPublic = true;
+    @Nested
+    @DisplayName("of 메서드 테스트")
+    class ofMethodTest {
+        @ParameterizedTest
+        @ValueSource(strings = {"content is null", "interviewCategory is null"})
+        @DisplayName("of 메서드에서 필드 값이 null이면 예외를 던집니다.")
+        void throwsExceptionWhenFieldValueIsNull(String fieldName) {
+            // given
+            String content = fieldName.equals("content is null") ? null : "content";
+            InterviewCategory interviewCategory = fieldName.equals("interviewCategory is null") ? null : InterviewCategory.PERSONAL;
+            boolean isPublic = true;
 
-        // when & then
-        assertThatThrownBy(() -> InterviewQuestion.of(content, interviewCategory, isPublic))
-                .isInstanceOf(NullPointerException.class);
+            // when & then
+            assertThatThrownBy(() -> InterviewQuestion.of(content, interviewCategory, isPublic))
+                    .isInstanceOf(NullPointerException.class);
+        }
+
+        @Test
+        @DisplayName("of 메서드에서 content가 blank이면 예외를 던집니다.")
+        void throwsExceptionWhenContentIsBlank() {
+            // given
+            String content = " ";
+            InterviewCategory interviewCategory = InterviewCategory.PERSONAL;
+            boolean isPublic = true;
+
+            // when & then
+            assertThatThrownBy(() -> InterviewQuestion.of(content, interviewCategory, isPublic))
+                    .isInstanceOf(InvalidInterviewQuestionContentException.class);
+        }
+
+        @Test
+        @DisplayName("of 메서드에서 필드 값이 정상이면 InterviewQuestion 객체를 생성합니다.")
+        void createInterviewQuestionObjectWhenFieldValuesAreValid() {
+            // given
+            String content = "content";
+            InterviewCategory interviewCategory = InterviewCategory.PERSONAL;
+            boolean isPublic = true;
+
+            // when
+            InterviewQuestion interviewQuestion = InterviewQuestion.of(content, interviewCategory, isPublic);
+
+            // then
+            assertThat(interviewQuestion).isNotNull();
+        }
     }
 
-    @Test
-    @DisplayName("of 메서드에서 content가 blank이면 예외를 던집니다.")
-    void throwsExceptionWhenContentIsBlank() {
-        // given
-        String content = " ";
-        InterviewCategory interviewCategory = InterviewCategory.PERSONAL;
-        boolean isPublic = true;
+    @Nested
+    @DisplayName("update 메서드 테스트")
+    class updateMethodTest {
 
-        // when & then
-        assertThatThrownBy(() -> InterviewQuestion.of(content, interviewCategory, isPublic))
-                .isInstanceOf(InvalidInterviewQuestionContentException.class);
-    }
+        @ParameterizedTest
+        @ValueSource(strings = {"content is null", "interviewCategory is null"})
+        @DisplayName("update 메서드에서 필드 값이 null이면 예외를 던집니다.")
+        void throwsExceptionWhenFieldValueIsNull(String fieldName) {
+            // given
+            InterviewQuestion interviewQuestion = InterviewQuestion.of("content", InterviewCategory.PERSONAL, true);
+            String content = fieldName.equals("content is null") ? null : "updated content";
+            InterviewCategory interviewCategory = fieldName.equals("interviewCategory is null") ? null : InterviewCategory.SOCIAL;
+            boolean isPublic = false;
 
-    @Test
-    @DisplayName("of 메서드에서 필드 값이 정상이면 InterviewQuestion 객체를 생성합니다.")
-    void createInterviewQuestionObjectWhenFieldValuesAreValid() {
-        // given
-        String content = "content";
-        InterviewCategory interviewCategory = InterviewCategory.PERSONAL;
-        boolean isPublic = true;
+            // when & then
+            assertThatThrownBy(() -> interviewQuestion.update(content, interviewCategory, isPublic))
+                    .isInstanceOf(NullPointerException.class);
+        }
 
-        // when
-        InterviewQuestion interviewQuestion = InterviewQuestion.of(content, interviewCategory, isPublic);
+        @Test
+        @DisplayName("update 메서드에서 content가 blank이면 예외를 던집니다.")
+        void throwsExceptionWhenContentIsBlank() {
+            // given
+            InterviewQuestion interviewQuestion = InterviewQuestion.of("content", InterviewCategory.PERSONAL, true);
+            String content = " ";
+            InterviewCategory interviewCategory = InterviewCategory.SOCIAL;
+            boolean isPublic = false;
 
-        // then
-        assertThat(interviewQuestion).isNotNull();
+            // when & then
+            assertThatThrownBy(() -> interviewQuestion.update(content, interviewCategory, isPublic))
+                    .isInstanceOf(InvalidInterviewQuestionContentException.class);
+        }
+
+        @Test
+        @DisplayName("update 메서드에서 필드 값이 정상이면 InterviewQuestion 객체를 업데이트 합니다.")
+        void createInterviewQuestionObjectWhenFieldValuesAreValid() {
+            // given
+            InterviewQuestion interviewQuestion = InterviewQuestion.of("content", InterviewCategory.PERSONAL, true);
+            String content = "updated content";
+            InterviewCategory interviewCategory = InterviewCategory.SOCIAL;
+            boolean isPublic = false;
+
+            // when
+            interviewQuestion.update(content, interviewCategory, isPublic);
+
+            // then
+            assertThat(interviewQuestion).isNotNull();
+            assertThat(interviewQuestion.getContent()).isEqualTo(content);
+            assertThat(interviewQuestion.isPublic()).isEqualTo(isPublic);
+        }
     }
 }
