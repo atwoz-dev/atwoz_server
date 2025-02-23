0ffbbf2be7d1ed3733b63d9393ed9ff22d05b76e
공태현 <dev0taehyeon@gmail.com>
2025-02-23 21:03:11 +0900

    refac : 프로필 이미지 업로드 시, 결과를 반환할 수 있도록 변경

--- src/main/java/atwoz/atwoz/member/presentation/profileimage/ProfileImageController.java
+++ src/main/java/atwoz/atwoz/member/presentation/profileimage/ProfileImageController.java
@@ -5,6 +5,7 @@ import atwoz.atwoz.auth.presentation.AuthPrincipal;
 import atwoz.atwoz.common.enums.StatusType;
 import atwoz.atwoz.common.response.BaseResponse;
 import atwoz.atwoz.member.command.application.profileImage.ProfileImageService;
+import atwoz.atwoz.member.command.application.profileImage.dto.ProfileImageUploadResponse;
 import atwoz.atwoz.member.presentation.profileimage.dto.ProfileImageUploadRequestWrapper;
 import atwoz.atwoz.member.query.profileimage.ProfileImageQueryRepository;
 import atwoz.atwoz.member.query.profileimage.view.ProfileImageView;
@@ -25,9 +26,8 @@ public class ProfileImageController {
     private final ProfileImageQueryRepository profileImageQueryRepository;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<BaseResponse<Void>> updateProfileImage(@ModelAttribute @Valid ProfileImageUploadRequestWrapper request, @AuthPrincipal AuthContext authContext) {
-        profileImageService.save(authContext.getId(), request.getRequests());
-        return ResponseEntity.ok(BaseResponse.from(StatusType.OK));
+    public ResponseEntity<BaseResponse<List<ProfileImageUploadResponse>>> updateProfileImage(@ModelAttribute @Valid ProfileImageUploadRequestWrapper request, @AuthPrincipal AuthContext authContext) {
+        return ResponseEntity.ok(BaseResponse.of(StatusType.OK, profileImageService.save(authContext.getId(), request.getRequests())));
     }
 
     @DeleteMapping("/{id}")
