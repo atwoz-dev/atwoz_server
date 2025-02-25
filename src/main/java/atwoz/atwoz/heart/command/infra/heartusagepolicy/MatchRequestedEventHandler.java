8505bd2173f030365b86d1b3779435d37968ba3d
hainho <hainho9704@gmail.com>
2025-02-26 08:26:42 +0900

    feat: match request event 핸들러 구현

---
+++ src/main/java/atwoz/atwoz/heart/command/infra/heartusagepolicy/MatchRequestedEventHandler.java
@@ -0,0 +1,19 @@
+package atwoz.atwoz.heart.command.infra.heartusagepolicy;
+
+import atwoz.atwoz.heart.command.application.heartusagepolicy.HeartUsageService;
+import atwoz.atwoz.heart.command.domain.hearttransaction.vo.TransactionType;
+import atwoz.atwoz.match.command.domain.match.event.MatchRequestedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MatchRequestedEventHandler {
+    private final HeartUsageService heartUsageService;
+
+    @EventListener(value = MatchRequestedEvent.class)
+    public void handle(MatchRequestedEvent event) {
+        heartUsageService.useHeart(event.getRequesterId(), TransactionType.MESSAGE);
+    }
+}
