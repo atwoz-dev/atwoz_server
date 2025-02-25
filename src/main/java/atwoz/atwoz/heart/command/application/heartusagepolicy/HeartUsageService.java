a12af6f88fdc7c89a8fb3221f2bf6621ed354172
hainho <hainho9704@gmail.com>
2025-02-26 08:26:07 +0900

    refac: member id를 받아서 하트 사용 처리하도록 수정

--- src/main/java/atwoz/atwoz/heart/command/application/heartusagepolicy/HeartUsageService.java
+++ src/main/java/atwoz/atwoz/heart/command/application/heartusagepolicy/HeartUsageService.java
@@ -2,8 +2,7 @@ package atwoz.atwoz.heart.command.application.heartusagepolicy;
 
 import atwoz.atwoz.heart.command.domain.hearttransaction.HeartTransaction;
 import atwoz.atwoz.heart.command.domain.hearttransaction.vo.TransactionType;
-import atwoz.atwoz.member.command.domain.member.Member;
 
 public interface HeartUsageService {
-    HeartTransaction useHeart(Member member, TransactionType transactionType);
+    HeartTransaction useHeart(long memberId, TransactionType transactionType);
 }
