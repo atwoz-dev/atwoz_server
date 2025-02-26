e4bc1c182f5db154916986b70017b39397f4605c
hainho <hainho9704@gmail.com>
2025-02-26 20:31:57 +0900

    refac: 하트 사용 핸들러 하나에서 하트 사용 처리 필요한 이벤트 모두 처리

--- src/main/java/atwoz/atwoz/heart/command/infra/heartusagepolicy/HeartUsageEventHandler.java
+++ src/main/java/atwoz/atwoz/heart/command/infra/heartusagepolicy/HeartUsageEventHandler.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MatchRequestedEventHandler {
+public class HeartUsageEventHandler {
     private final HeartUsagePolicyService heartUsageService;
 
     @EventListener(value = MatchRequestedEvent.class)
