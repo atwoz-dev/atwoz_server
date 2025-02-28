84bceb1da5e8d7038a737721b77b2d5eceec5d27
hoyunjung <jhy9094@gmail.com>
2025-02-28 14:06:56 +0900

    feat: Firebase 추가
    #102

---
+++ src/main/java/atwoz/atwoz/notification/infra/FirebaseConfig.java
@@ -0,0 +1,22 @@
+package atwoz.atwoz.notification.infra;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    private void init() throws IOException {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
