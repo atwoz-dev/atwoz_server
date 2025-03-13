0958d83700fe37eea9494e53a549573ac456d6ed
공태현 <dev0taehyeon@gmail.com>
2025-03-13 09:42:58 +0900

    feat : 테스트 케이스 추가

--- src/main/java/atwoz/atwoz/community/command/application/selfintroduction/SelfIntroductionService.java
+++ src/main/java/atwoz/atwoz/community/command/application/selfintroduction/SelfIntroductionService.java
@@ -3,6 +3,8 @@ package atwoz.atwoz.community.command.application.selfintroduction;
 import atwoz.atwoz.community.command.domain.selfintroduction.SelfIntroduction;
 import atwoz.atwoz.community.command.domain.selfintroduction.SelfIntroductionCommandRepository;
 import atwoz.atwoz.community.presentation.selfintroduction.dto.SelfIntroductionWriteRequest;
+import atwoz.atwoz.member.command.application.member.exception.MemberNotFoundException;
+import atwoz.atwoz.member.command.domain.member.MemberCommandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +13,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SelfIntroductionService {
     private final SelfIntroductionCommandRepository selfIntroductionCommandRepository;
+    private final MemberCommandRepository memberCommandRepository;
 
     @Transactional
     public void write(SelfIntroductionWriteRequest request, Long memberId) {
+        validateMemberId(memberId);
         SelfIntroduction selfIntroduction = SelfIntroduction.write(memberId, request.title(), request.content());
         selfIntroductionCommandRepository.save(selfIntroduction);
+    }
+
+    private void validateMemberId(Long memberId) {
+        memberCommandRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
     }
 }
--- src/test/java/atwoz/atwoz/community/command/application/selfintroduction/SelfIntroductionServiceTest.java
+++ src/test/java/atwoz/atwoz/community/command/application/selfintroduction/SelfIntroductionServiceTest.java
@@ -2,12 +2,19 @@ package atwoz.atwoz.community.command.application.selfintroduction;
 
 import atwoz.atwoz.community.command.domain.selfintroduction.SelfIntroductionCommandRepository;
 import atwoz.atwoz.community.presentation.selfintroduction.dto.SelfIntroductionWriteRequest;
+import atwoz.atwoz.member.command.application.member.exception.MemberNotFoundException;
+import atwoz.atwoz.member.command.domain.member.Member;
+import atwoz.atwoz.member.command.domain.member.MemberCommandRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
@@ -18,8 +25,26 @@ public class SelfIntroductionServiceTest {
     @Mock
     private SelfIntroductionCommandRepository selfIntroductionCommandRepository;
 
+    @Mock
+    private MemberCommandRepository memberCommandRepository;
+
     @InjectMocks
     private SelfIntroductionService selfIntroductionService;
+
+    @DisplayName("존재하지 않는 멤버의 ID인 경우, 예외 발생")
+    @Test
+    void throwExceptionWhenMemberNotFound() {
+        // Given
+        Long memberId = 1L;
+        String title = "셀프 소개 제목";
+        String content = "셀프 소개 내용입니다. 최소 내용이 30자 이상입니다~!!! (30자 이상)";
+
+        Mockito.when(memberCommandRepository.findById(memberId)).thenReturn(Optional.empty());
+
+        // When & Then
+        Assertions.assertThatThrownBy(() -> selfIntroductionService.write(new SelfIntroductionWriteRequest(title, content), memberId))
+                .isInstanceOf(MemberNotFoundException.class);
+    }
 
     @DisplayName("셀프 소개를 작성한다.")
     @Test
@@ -28,6 +53,8 @@ public class SelfIntroductionServiceTest {
         Long memberId = 1L;
         String title = "셀프 소개 제목";
         String content = "셀프 소개 내용입니다. 최소 내용이 30자 이상입니다~!!! (30자 이상)";
+
+        Mockito.when(memberCommandRepository.findById(memberId)).thenReturn(Optional.of(Mockito.mock(Member.class)));
 
         // When
         selfIntroductionService.write(new SelfIntroductionWriteRequest(title, content), memberId);
