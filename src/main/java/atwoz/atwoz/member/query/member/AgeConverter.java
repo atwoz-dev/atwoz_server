a1e1322291f7ad7c701bfb89f3482d8b127e0079
공태현 <dev0taehyeon@gmail.com>
2025-03-28 21:41:05 +0900

    fix : 나이-연도 변환 로직을 View 에서 제거.

--- src/main/java/atwoz/atwoz/member/query/member/AgeConverter.java
+++ src/main/java/atwoz/atwoz/member/query/member/AgeConverter.java
@@ -4,7 +4,13 @@ import java.util.Calendar;
 
 public class AgeConverter {
     public static Integer toAge(Integer yearOfBirth) {
-        return yearOfBirth == null ? null : yearOfBirth - Calendar.getInstance().get(Calendar.YEAR) + 1;
+
+        int current = Calendar.getInstance().get(Calendar.YEAR);
+        int age = current - yearOfBirth + 1;
+        System.out.println("current : " + current);
+        System.out.println("age : " + age);
+        System.out.println("year : " + (current - age - 1));
+        return yearOfBirth == null ? null : Calendar.getInstance().get(Calendar.YEAR) - yearOfBirth + 1;
     }
 
     public static Integer toYearOfBirth(Integer age) {
