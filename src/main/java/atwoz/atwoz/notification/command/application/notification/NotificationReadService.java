a069f829c5f9d02fdabac6b3b4d188db97cee927
hoyunjung <jhy9094@gmail.com>
2025-04-03 12:05:11 +0900

    feat: 알림 읽기 기능 개발
    #131

---
+++ src/main/java/atwoz/atwoz/notification/command/application/notification/NotificationReadService.java
@@ -0,0 +1,25 @@
+package atwoz.atwoz.notification.command.application.notification;
+
+import atwoz.atwoz.notification.command.domain.notification.Notification;
+import atwoz.atwoz.notification.command.domain.notification.NotificationCommandRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationReadService {
+
+    private final NotificationCommandRepository notificationCommandRepository;
+
+    @Transactional
+    public void markAsRead(long notificationId) {
+        Notification notification = getNotification(notificationId);
+        notification.markAsRead();
+    }
+
+    private Notification getNotification(long notificationId) {
+        return notificationCommandRepository.findById(notificationId)
+                .orElseThrow(() -> new NotificationNotFoundException(notificationId));
+    }
+}
--- src/main/java/atwoz/atwoz/notification/presentation/notification/NotificationController.java
+++ src/main/java/atwoz/atwoz/notification/presentation/notification/NotificationController.java
@@ -1,14 +1,12 @@
 package atwoz.atwoz.notification.presentation.notification;
 
 import atwoz.atwoz.common.response.BaseResponse;
+import atwoz.atwoz.notification.command.application.notification.NotificationReadService;
 import atwoz.atwoz.notification.command.application.notification.NotificationSendService;
 import atwoz.atwoz.notification.infra.notification.NotificationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static atwoz.atwoz.common.enums.StatusType.OK;
 
@@ -18,16 +16,21 @@ import static atwoz.atwoz.common.enums.StatusType.OK;
 public class NotificationController {
 
     private final NotificationSendService notificationSendService;
+    private final NotificationReadService notificationReadService;
 
     /**
      * 알림 테스트를 위한 메서드입니다.
      */
     // TODO: 테스트 후 삭제
     @PostMapping("/test")
-    public ResponseEntity<BaseResponse<Void>> updateDeviceToken(
-            @RequestBody NotificationRequest request
-    ) {
+    public ResponseEntity<BaseResponse<Void>> updateDeviceToken(@RequestBody NotificationRequest request) {
         notificationSendService.send(request);
+        return ResponseEntity.ok(BaseResponse.from(OK));
+    }
+
+    @PatchMapping("/{notificationId}")
+    public ResponseEntity<BaseResponse<Void>> markAsRead(@PathVariable long notificationId) {
+        notificationReadService.markAsRead(notificationId);
         return ResponseEntity.ok(BaseResponse.from(OK));
     }
 }
