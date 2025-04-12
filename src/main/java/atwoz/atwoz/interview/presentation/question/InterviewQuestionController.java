2d78f6001dbf493f52046c713df7d9c00be77c58
hainho <hainho9704@gmail.com>
2025-04-13 06:08:53 +0900

    refac: 어드민 인터뷰 조회와 앱 인터뷰 조회 분리

--- src/main/java/atwoz/atwoz/interview/presentation/question/InterviewQuestionController.java
+++ src/main/java/atwoz/atwoz/interview/presentation/question/InterviewQuestionController.java
@@ -4,11 +4,8 @@ import atwoz.atwoz.auth.presentation.AuthContext;
 import atwoz.atwoz.auth.presentation.AuthPrincipal;
 import atwoz.atwoz.common.enums.StatusType;
 import atwoz.atwoz.common.response.BaseResponse;
-import atwoz.atwoz.interview.command.application.question.InterviewQuestionService;
-import atwoz.atwoz.interview.presentation.question.dto.InterviewQuestionSaveRequest;
 import atwoz.atwoz.interview.query.question.InterviewQuestionQueryRepository;
 import atwoz.atwoz.interview.query.question.view.InterviewQuestionView;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,24 +14,11 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/interview/question")
 public class InterviewQuestionController {
-
-    private final InterviewQuestionService interviewQuestionService;
     private final InterviewQuestionQueryRepository interviewQuestionQueryRepository;
 
-    @PostMapping("/admin/interview/question")
-    public ResponseEntity<BaseResponse<Void>> createQuestion(@Valid @RequestBody InterviewQuestionSaveRequest request) {
-        interviewQuestionService.createQuestion(request);
-        return ResponseEntity.ok(BaseResponse.from(StatusType.OK));
-    }
-
-    @PatchMapping("/admin/interview/question/{id}")
-    public ResponseEntity<BaseResponse<Void>> updateQuestion(@PathVariable Long id, @Valid @RequestBody InterviewQuestionSaveRequest request) {
-        interviewQuestionService.updateQuestion(id, request);
-        return ResponseEntity.ok(BaseResponse.from(StatusType.OK));
-    }
-
-    @GetMapping("/interview/question")
+    @GetMapping
     public ResponseEntity<BaseResponse<List<InterviewQuestionView>>> getQuestionAllByCategory(
             @RequestParam String category,
             @AuthPrincipal AuthContext authContext
