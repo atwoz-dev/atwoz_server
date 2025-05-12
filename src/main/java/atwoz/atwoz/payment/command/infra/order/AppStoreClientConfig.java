fdac0b4fb1d463a0dc3a60bc66ebd44343baea78
hainho <hainho9704@gmail.com>
2025-05-12 14:55:00 +0900

    feat: 앱스토어 api response 검증 및 decode 위한 SignedDataVerifier 빈 추가
    - Add `app-apple-id` and `root-ca-paths` configurations
    - Implement `SignedDataVerifier` bean in `AppStoreClientConfig`

--- src/main/java/atwoz/atwoz/payment/command/infra/order/AppStoreClientConfig.java
+++ src/main/java/atwoz/atwoz/payment/command/infra/order/AppStoreClientConfig.java
@@ -3,11 +3,19 @@ package atwoz.atwoz.payment.command.infra.order;
 import com.apple.itunes.storekit.client.AppStoreServerAPIClient;
 import com.apple.itunes.storekit.migration.ReceiptUtility;
 import com.apple.itunes.storekit.model.Environment;
+import com.apple.itunes.storekit.verification.SignedDataVerifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
 public class AppStoreClientConfig {
@@ -28,5 +36,27 @@ public class AppStoreClientConfig {
     @Bean
     public ReceiptUtility receiptUtility() {
         return new ReceiptUtility();
+    }
+
+    @Bean
+    public SignedDataVerifier signedDataVerifier(
+        @Value("${payment.app-store.root-ca-paths}") String[] rootCaPaths,
+        @Value("${payment.app-store.bundle-id}") String bundleId,
+        @Value("${payment.app-store.app-apple-id}") Long appAppleId,
+        @Value("${payment.app-store.environment}") String environmentValue) {
+        Environment environment = Environment.fromValue(environmentValue);
+        Set<InputStream> rootCAs = Arrays.stream(rootCaPaths)
+            .map(path -> {
+                try {
+                    return new FileInputStream(path);
+                } catch (FileNotFoundException e) {
+                    throw new UncheckedIOException(e);
+                }
+            })
+            .collect(Collectors.toSet());
+        if (environment.equals(Environment.SANDBOX)) {
+            appAppleId = null;
+        }
+        return new SignedDataVerifier(rootCAs, bundleId, appAppleId, environment, true);
     }
 }
