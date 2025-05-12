8146e00a71799e8fbf89f4ce7bdb5832eaa2acdd
hainho <hainho9704@gmail.com>
2025-05-12 14:04:40 +0900

    feat: transactionId 입력 받는 대신 appReceipt를 받아 transactionId 추출하여 사용하도록 수

--- src/main/java/atwoz/atwoz/payment/command/infra/order/AppStoreClient.java
+++ src/main/java/atwoz/atwoz/payment/command/infra/order/AppStoreClient.java
@@ -1,9 +1,11 @@
 package atwoz.atwoz.payment.command.infra.order;
 
 import atwoz.atwoz.payment.command.infra.order.exception.AppStoreClientException;
+import atwoz.atwoz.payment.command.infra.order.exception.InvalidAppReceiptException;
 import atwoz.atwoz.payment.command.infra.order.exception.InvalidTransactionIdException;
 import com.apple.itunes.storekit.client.APIException;
 import com.apple.itunes.storekit.client.AppStoreServerAPIClient;
+import com.apple.itunes.storekit.migration.ReceiptUtility;
 import com.apple.itunes.storekit.model.TransactionInfoResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +16,25 @@ import org.springframework.stereotype.Component;
 public class AppStoreClient {
 
     private final AppStoreServerAPIClient client;
+    private final ReceiptUtility receiptUtil;
 
-    public TransactionInfoResponse getTransactionInfo(@NonNull String transactionId) {
+    public TransactionInfoResponse getTransactionInfo(@NonNull String appReceipt) {
         try {
+            String transactionId = getTransactionId(appReceipt);
             return client.getTransactionInfo(transactionId);
         } catch (APIException e) {
             handleAPIException(e);
             throw new AppStoreClientException(e);
         } catch (Exception e) {
             throw new AppStoreClientException(e);
+        }
+    }
+
+    private String getTransactionId(String appReceipt) {
+        try {
+            return receiptUtil.extractTransactionIdFromAppReceipt(appReceipt);
+        } catch (Exception e) {
+            throw new InvalidAppReceiptException(e);
         }
     }
 
