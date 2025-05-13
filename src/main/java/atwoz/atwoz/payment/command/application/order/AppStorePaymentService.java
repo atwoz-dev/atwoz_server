623f0b4a98f1d9713d809c962d145433af48d666
hainho <hainho9704@gmail.com>
2025-05-13 11:53:06 +0900

    feat(payment): 영수증 검증 이후 주문 처리 로직을 이벤트로 분리하고 별도의 트랙잭션으로 분
    - 추가된 이벤트 핸들러로 영수증 검증 처리 로직 수행
    - OrderService 분리로 주문 생성 및 하트 구매 로직 리팩토링
    - AppStoreReceiptVerifiedEvent 도입으로 이벤트 기반 처리 지원

--- src/main/java/atwoz/atwoz/payment/command/application/order/AppStorePaymentService.java
+++ src/main/java/atwoz/atwoz/payment/command/application/order/AppStorePaymentService.java
@@ -1,57 +1,43 @@
 package atwoz.atwoz.payment.command.application.order;
 
-import atwoz.atwoz.payment.command.application.order.exception.HeartPurchaseOptionNotFoundException;
+import atwoz.atwoz.common.event.Events;
 import atwoz.atwoz.payment.command.application.order.exception.InvalidOrderException;
-import atwoz.atwoz.payment.command.application.order.exception.OrderAlreadyExistsException;
-import atwoz.atwoz.payment.command.domain.heartpurchaseoption.HeartPurchaseOption;
-import atwoz.atwoz.payment.command.domain.heartpurchaseoption.HeartPurchaseOptionCommandRepository;
-import atwoz.atwoz.payment.command.domain.order.Order;
-import atwoz.atwoz.payment.command.domain.order.OrderCommandRepository;
-import atwoz.atwoz.payment.command.domain.order.PaymentMethod;
+import atwoz.atwoz.payment.command.domain.order.event.AppStoreReceiptVerifiedEvent;
 import atwoz.atwoz.payment.command.infra.order.AppStoreClient;
 import com.apple.itunes.storekit.model.JWSTransactionDecodedPayload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AppStorePaymentService {
     private final AppStoreClient appStoreClient;
-    private final OrderCommandRepository orderCommandRepository;
-    private final HeartPurchaseOptionCommandRepository heartPurchaseOptionCommandRepository;
 
-    @Transactional
     public void verifyReceipt(String appReceipt, Long memberId) {
         JWSTransactionDecodedPayload decodedPayload = getTransactionDecodedPayload(appReceipt);
-        verifyTransactionInfo(decodedPayload);
-        createOrder(memberId, decodedPayload);
-        purchaseHeart(memberId, decodedPayload.getProductId(), decodedPayload.getQuantity());
+        verifyPayload(decodedPayload);
+        raiseReceiptVerifiedEvent(memberId, decodedPayload);
     }
 
     private JWSTransactionDecodedPayload getTransactionDecodedPayload(String appReceipt) {
         return appStoreClient.getTransactionDecodedPayload(appReceipt);
     }
 
-    private void verifyTransactionInfo(JWSTransactionDecodedPayload decodedPayload) {
+    private void verifyPayload(JWSTransactionDecodedPayload decodedPayload) {
         if (decodedPayload.getRevocationDate() != null) {
             throw new InvalidOrderException();
         }
-        if (orderCommandRepository.existsByTransactionIdAndPaymentMethod(decodedPayload.getTransactionId(),
-            PaymentMethod.APP_STORE)) {
-            throw new OrderAlreadyExistsException();
-        }
     }
 
-    private void createOrder(Long memberId, JWSTransactionDecodedPayload decodedPayload) {
-        Order order = Order.of(memberId, decodedPayload.getTransactionId(), PaymentMethod.APP_STORE);
-        orderCommandRepository.save(order);
-    }
-
-    private void purchaseHeart(Long memberId, String productId, Integer quantity) {
-        HeartPurchaseOption heartPurchaseOption = heartPurchaseOptionCommandRepository.findByProductId(productId)
-            .orElseThrow(
-                () -> new HeartPurchaseOptionNotFoundException("하트 구매 옵션이 존재하지 않습니다. product id:" + productId));
-        heartPurchaseOption.purchase(memberId, quantity);
+    private void raiseReceiptVerifiedEvent(final Long memberId,
+        final JWSTransactionDecodedPayload decodedPayload) {
+        Events.raise(
+            AppStoreReceiptVerifiedEvent.of(
+                memberId,
+                decodedPayload.getTransactionId(),
+                decodedPayload.getProductId(),
+                decodedPayload.getQuantity()
+            )
+        );
     }
 }
