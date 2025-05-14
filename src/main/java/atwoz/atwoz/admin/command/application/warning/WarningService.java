879dcf68fb5be199fc047872b43fea2e62fd7cc3
hoyunjung <jhy9094@gmail.com>
2025-05-14 17:46:02 +0900

    fix: 경고 발행 메서드 리팩토링 및 로그 추가
    - 경고 생성 메서드 이름 변경 및 반환 타입 수정
    - 경고 발행 시 로그 출력 추가
    
    #187

--- src/main/java/atwoz/atwoz/admin/command/application/warning/WarningService.java
+++ src/main/java/atwoz/atwoz/admin/command/application/warning/WarningService.java
@@ -17,8 +17,10 @@ public class WarningService {
     private final WarningCommandRepository warningCommandRepository;
 
     @Transactional
-    public void create(long adminId, WarningCreatedRequest request) {
-        Warning warning = Warning.of(adminId, request.memberId(), toWarningReasonType(request.reasonType()));
+    public Long issue(long adminId, WarningCreatedRequest request) {
+        var warning = Warning.of(adminId, request.memberId(), toWarningReasonType(request.reasonType()));
         warningCommandRepository.save(warning);
+        log.info("멤버 id: {}에 대한 경고 발행(id: {})", warning.getMemberId(), warning.getId());
+        return warning.getId();
     }
 }
