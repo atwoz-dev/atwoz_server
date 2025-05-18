c842eb3ba8cbf1ea21382ed541da3eac788f7c28
hainho <hainho9704@gmail.com>
2025-05-18 20:16:36 +0900

    feat: add descriptions to TransactionType and integrate with HeartTransaction
    - TransactionType now includes a description field
    - Updated HeartTransaction creation to use TransactionType descriptions

--- src/main/java/atwoz/atwoz/heart/command/domain/hearttransaction/vo/TransactionType.java
+++ src/main/java/atwoz/atwoz/heart/command/domain/hearttransaction/vo/TransactionType.java
@@ -1,18 +1,25 @@
 package atwoz.atwoz.heart.command.domain.hearttransaction.vo;
 
 
+import lombok.Getter;
+
 public enum TransactionType {
-    MISSION,
-    MESSAGE,
-    DATING_EXAM,
-    PROFILE_EXCHANGE,
-    SELF_INTRODUCTION,
-    INTRODUCTION,
-    PURCHASE;
+    MISSION("미션 완료"),
+    MESSAGE("메시지 보내기"),
+    DATING_EXAM("연애 모의고사 다시보기"),
+    PROFILE_EXCHANGE("프로필 교환 신청"),
+    INTRODUCTION("소개 받기"),
+    PURCHASE("하트 구매");
+
+    @Getter
+    private final String description;
+
+    TransactionType(String description) {
+        this.description = description;
+    }
 
     public boolean isUsingType() {
-        return this == MESSAGE || this == DATING_EXAM || this == PROFILE_EXCHANGE || this == SELF_INTRODUCTION ||
-            this == INTRODUCTION;
+        return this == MESSAGE || this == DATING_EXAM || this == PROFILE_EXCHANGE || this == INTRODUCTION;
     }
 
     public boolean isGainingType() {
