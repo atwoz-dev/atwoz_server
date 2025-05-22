6724e7216ca4657874b4257a18a4f3bafd9e2a44
hainho <hainho9704@gmail.com>
2025-05-22 20:43:52 +0900

    refactor: 제거 불필요한 Swagger 어노테이션
    Swagger 어노테이션 관련 코드 정리

--- src/main/java/atwoz/atwoz/interview/command/domain/question/InterviewCategory.java
+++ src/main/java/atwoz/atwoz/interview/command/domain/question/InterviewCategory.java
@@ -1,13 +1,7 @@
 package atwoz.atwoz.interview.command.domain.question;
 
 import atwoz.atwoz.interview.command.domain.question.exception.InvalidInterviewCategoryException;
-import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(
-    description = "인터뷰 질문 카테고리",
-    example = "PERSONAL",
-    type = "string"
-)
 public enum InterviewCategory {
     PERSONAL("나"),
     SOCIAL("관계"),
