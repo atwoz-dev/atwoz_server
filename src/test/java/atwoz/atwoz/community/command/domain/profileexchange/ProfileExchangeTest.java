61ccf18ac3c06dafa251f767061dc2219aacbfcb
공태현 <dev0taehyeon@gmail.com>
2025-05-27 14:41:00 +0900

    [refac] : 테스트 메소드명 수정.

--- src/test/java/atwoz/atwoz/community/command/domain/profileexchange/ProfileExchangeTest.java
+++ src/test/java/atwoz/atwoz/community/command/domain/profileexchange/ProfileExchangeTest.java
@@ -11,7 +11,7 @@ class ProfileExchangeTest {
 
     @Test
     @DisplayName("")
-    void throwExceptionWhenRequesterIdIsNullAndResponderIdIsNull() {
+    void throwExceptionWhenRequesterIdIsEqualToResponderId() {
         // Given
         long requestId = 1L;
 
