76901a41ff3240baa908b26aea58872a84767795
hoyunjung <jhy9094@gmail.com>
2025-05-29 21:50:40 +0900

    test: Mock 객체 관련 테스트 코드 수정
    - MemberCommandRepository 주입 및 연관 Mock 객체 추가
    - LikeSendServiceTest 내 send 메서드 테스트 로직 강화
    
    #225

--- src/test/java/atwoz/atwoz/like/command/application/LikeSendServiceTest.java
+++ src/test/java/atwoz/atwoz/like/command/application/LikeSendServiceTest.java
@@ -4,6 +4,10 @@ import atwoz.atwoz.like.command.domain.Like;
 import atwoz.atwoz.like.command.domain.LikeCommandRepository;
 import atwoz.atwoz.like.presentation.LikeLevelRequest;
 import atwoz.atwoz.like.presentation.LikeSendRequest;
+import atwoz.atwoz.member.command.domain.member.Member;
+import atwoz.atwoz.member.command.domain.member.MemberCommandRepository;
+import atwoz.atwoz.member.command.domain.member.vo.MemberProfile;
+import atwoz.atwoz.member.command.domain.member.vo.Nickname;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,19 +15,25 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class LikeSendServiceTest {
 
-    @InjectMocks
-    private LikeSendService likeSendService;
+    @Mock
+    LikeCommandRepository likeCommandRepository;
 
     @Mock
-    private LikeCommandRepository likeCommandRepository;
+    MemberCommandRepository memberCommandRepository;
+
+    @InjectMocks
+    LikeSendService likeSendService;
 
     @Test
     @DisplayName("좋아요를 성공적으로 보낸다.")
@@ -35,6 +45,18 @@ class LikeSendServiceTest {
         var request = new LikeSendRequest(receiverId, likeLevel);
 
         given(likeCommandRepository.existsBySenderIdAndReceiverId(senderId, receiverId)).willReturn(false);
+
+        var mockNickname = mock(Nickname.class);
+        given(mockNickname.getValue()).willReturn("nickname");
+
+        var mockProfile = mock(MemberProfile.class);
+        given(mockProfile.getNickname()).willReturn(mockNickname);
+
+        var mockMember = mock(Member.class);
+        given(mockMember.getProfile()).willReturn(mockProfile);
+
+        given(memberCommandRepository.findById(senderId))
+            .willReturn(Optional.of(mockMember));
 
         // when
         likeSendService.send(senderId, request);
