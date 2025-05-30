b5da843f46332a759f8f608eb87bf649108693dd
hoyunjung <jhy9094@gmail.com>
2025-05-30 22:54:29 +0900

    feat(suspension): Swagger 문서화 추가
    - SuspensionController에 @Tag 및 @Operation 적용
    
    #188

--- src/main/java/atwoz/atwoz/admin/presentation/suspension/SuspensionController.java
+++ src/main/java/atwoz/atwoz/admin/presentation/suspension/SuspensionController.java
@@ -5,6 +5,8 @@ import atwoz.atwoz.admin.command.application.suspension.SuspensionService;
 import atwoz.atwoz.auth.presentation.AuthContext;
 import atwoz.atwoz.auth.presentation.AuthPrincipal;
 import atwoz.atwoz.common.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static atwoz.atwoz.common.enums.StatusType.OK;
 
+@Tag(name = "관리자 회원 정지 API")
 @RestController
 @RequestMapping("/admin/suspensions")
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class SuspensionController {
 
     private final SuspensionService suspensionService;
 
+    @Operation(description = "회원 정지 상태 변경")
     @PostMapping
     public ResponseEntity<BaseResponse<Void>> updateStatus(
         @Valid @RequestBody SuspendRequest request,
