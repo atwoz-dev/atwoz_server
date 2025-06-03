58eff50e01a4d7550e1a9ea6a4db954f93c633d4
hainho <hainho9704@gmail.com>
2025-06-03 13:25:31 +0900

    docs: payment 패키지 swagger convention 적용

--- src/main/java/atwoz/atwoz/payment/presentation/heartpurchaseoption/AdminHeartPurchaseOptionController.java
+++ src/main/java/atwoz/atwoz/payment/presentation/heartpurchaseoption/AdminHeartPurchaseOptionController.java
@@ -7,6 +7,8 @@ import atwoz.atwoz.payment.presentation.heartpurchaseoption.dto.HeartPurchaseOpt
 import atwoz.atwoz.payment.query.heartpurchaseoption.HeartPurchaseOptionQueryRepository;
 import atwoz.atwoz.payment.query.heartpurchaseoption.condition.HeartPurchaseOptionSearchCondition;
 import atwoz.atwoz.payment.query.heartpurchaseoption.view.HeartPurchaseOptionView;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,6 +17,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "어드민 하트 구매 옵션 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin/heart-purchase-options")
@@ -22,19 +25,21 @@ public class AdminHeartPurchaseOptionController {
     private final HeartPurchaseOptionService heartPurchaseOptionService;
     private final HeartPurchaseOptionQueryRepository heartPurchaseOptionQueryRepository;
 
+    @Operation(summary = "하트 구매 옵션 생성")
     @PostMapping
     public ResponseEntity<BaseResponse<Void>> create(@Valid @RequestBody HeartPurchaseOptionCreateRequest request) {
         heartPurchaseOptionService.create(request);
         return ResponseEntity.ok(BaseResponse.from(StatusType.OK));
     }
 
-
+    @Operation(summary = "하트 구매 옵션 삭제")
     @DeleteMapping("/{id}")
     public ResponseEntity<BaseResponse<Void>> delete(@PathVariable Long id) {
         heartPurchaseOptionService.delete(id);
         return ResponseEntity.ok(BaseResponse.from(StatusType.OK));
     }
 
+    @Operation(summary = "하트 구매 옵션 목록 조회")
     @GetMapping
     public ResponseEntity<BaseResponse<Page<HeartPurchaseOptionView>>> getPage(
         @ModelAttribute HeartPurchaseOptionSearchCondition condition,
