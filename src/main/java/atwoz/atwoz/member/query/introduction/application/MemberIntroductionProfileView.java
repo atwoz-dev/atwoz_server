07be0a6c45116f95b32c6c46fdf7268b0d86034c
hainho <hainho9704@gmail.com>
2025-06-03 12:47:31 +0900

    docs: admin 패키지 Swagger 태그 및 API 설명 추가
    - 관리자 심사 관리 API에 @Tag 및 @Operation 추가
    - 관리자 인증 API에 @Tag 및 @Operation 추가
    - 관리자 멤버 관리 API에 @Tag 및 @Operation 추가
    - MemberIntroductionProfileView에 @ArraySchema 추가

--- src/main/java/atwoz/atwoz/member/query/introduction/application/MemberIntroductionProfileView.java
+++ src/main/java/atwoz/atwoz/member/query/introduction/application/MemberIntroductionProfileView.java
@@ -4,6 +4,7 @@ import atwoz.atwoz.like.command.domain.LikeLevel;
 import atwoz.atwoz.member.command.domain.member.Hobby;
 import atwoz.atwoz.member.command.domain.member.Mbti;
 import atwoz.atwoz.member.command.domain.member.Religion;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 public record MemberIntroductionProfileView(
     long memberId,
     String profileImageUrl,
-    @Schema(implementation = Hobby.class)
+    @ArraySchema(schema = @Schema(implementation = Hobby.class))
     List<String> hobbies,
     @Schema(implementation = Mbti.class)
     String mbti,
