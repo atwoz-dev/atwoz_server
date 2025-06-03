cc255c1008c748aa75ab1993bcbf48735fa766b7
hainho <hainho9704@gmail.com>
2025-06-03 13:02:24 +0900

    docs: community 패키지 swagger convention 적용
    - SelfIntroductionSearchRequest, SelfIntroductionView 등 DTO에 @Schema 및 @ArraySchema 어노테이션 추가
    - SelfIntroductionController에 API 설명을 위한 @Operation 및 @Tag 어노테이션 추가

--- src/main/java/atwoz/atwoz/community/presentation/selfintroduction/dto/SelfIntroductionSearchRequest.java
+++ src/main/java/atwoz/atwoz/community/presentation/selfintroduction/dto/SelfIntroductionSearchRequest.java
@@ -1,13 +1,18 @@
 package atwoz.atwoz.community.presentation.selfintroduction.dto;
 
 import atwoz.atwoz.member.command.domain.member.City;
+import atwoz.atwoz.member.command.domain.member.Gender;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record SelfIntroductionSearchRequest(
+    @ArraySchema(schema = @Schema(implementation = City.class))
     List<City> preferredCities,
     Integer fromAge,
     Integer toAge,
+    @Schema(implementation = Gender.class)
     String gender
 ) {
 }
