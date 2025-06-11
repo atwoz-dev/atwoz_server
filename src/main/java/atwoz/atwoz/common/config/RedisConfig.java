e9a38f8d89ab6d5e7c522569676ac8b794e163b7
Ha In Ho <hainho9704@gmail.com>
2025-06-11 15:28:02 +0900

    Redis Config 수정 (#239)
    * refac: redis config가 application yml 사용해서 RedisConnectionFactory 생성하도록 수정
    
    * fix: application yml에 redis 설정을 표준방식으로 변경

--- src/main/java/atwoz/atwoz/common/config/RedisConfig.java
+++ src/main/java/atwoz/atwoz/common/config/RedisConfig.java
@@ -1,34 +1,20 @@
 package atwoz.atwoz.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Value("${spring.data.redis.connection.port}")
-    private int port;
-    @Value("${spring.data.redis.connection.host}")
-    private String host;
-
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
 }
