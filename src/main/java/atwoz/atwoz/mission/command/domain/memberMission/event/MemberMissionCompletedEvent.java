5a91b4eeadd281918c366daadbc8e15dc849bd82
공태현 <dev0taehyeon@gmail.com>
2025-07-15 22:17:14 +0900

    fix : Event 상속 변경. (Events -> Event)

--- src/main/java/atwoz/atwoz/mission/command/domain/memberMission/event/MemberMissionCompletedEvent.java
+++ src/main/java/atwoz/atwoz/mission/command/domain/memberMission/event/MemberMissionCompletedEvent.java
@@ -1,12 +1,12 @@
 package atwoz.atwoz.mission.command.domain.memberMission.event;
 
-import atwoz.atwoz.common.event.Events;
+import atwoz.atwoz.common.event.Event;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
-public class MemberMissionCompletedEvent extends Events {
+public class MemberMissionCompletedEvent extends Event {
 
     private final long memberId;
 
