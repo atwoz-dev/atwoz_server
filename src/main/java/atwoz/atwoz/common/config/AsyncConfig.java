3d2cbecc69b32bd4a3ab2410c1c4a92e23a6e770
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2025-08-19 21:18:56 +0900

    @Async 스레드 풀 설정 (#297)
    * feat: 비동기 설정 추가
    
    - AsyncConfig 클래스 추가로 커스텀 비동기 설정 적용
    - @EnableAsync 어노테이션을 AsyncConfig로 이동하여 관리
    
    #296
    
    * chore: 종료 대기 시간 변경
    
    #297
    
    * refactor: Spring boot 기본 예외처리 핸들러 사용하도록 변경
    
    #296

---
+++ src/main/java/atwoz/atwoz/common/config/AsyncConfig.java
@@ -0,0 +1,30 @@
+package atwoz.atwoz.common.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Slf4j
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(50);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.setThreadNamePrefix("custom-async-");
+        executor.initialize();
+        return executor;
+    }
+}
