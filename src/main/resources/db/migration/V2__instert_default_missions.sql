b410023f5b012f7f2eed3484ca9d394ca2258da2
Ha In Ho <hainho9704@gmail.com>
2025-09-24 15:04:01 +0900

    fix: 미션 데이터 스크립트 수정 (#325)

--- src/main/resources/db/migration/V2__instert_default_missions.sql
+++ src/main/resources/db/migration/V2__instert_default_missions.sql
@@ -1,5 +1,6 @@
-INSERT INTO missions (created_at, updated_at, action_type, frequency_type, target_gender ,required_attempt, repeatable_count, rewarded_heart, is_public)
-VALUES (NOW(6), NOW(6), 'LIKE', 'DAILY', 'MALE', 1, 2, 2 ,1),
-       (NOW(6), NOW(6), 'LIKE', 'DAILY', 'MALE', 1, 3, 2 ,1),
-       (NOW(6), NOW(6), 'INTERVIEW', 'CHALLENGE', 'ALL', 3, 1, 10 ,1),
-       (NOW(6), NOW(6), 'FIRST_DATE_EXAM', 'CHALLENGE', 'ALL', 1, 1, 10 ,1)
+INSERT INTO missions (created_at, updated_at, action_type, frequency_type, target_gender, required_attempt,
+                      repeatable_count, rewarded_heart, is_public)
+VALUES (NOW(6), NOW(6), 'LIKE', 'DAILY', 'MALE', 1, 2, 2, 1),
+       (NOW(6), NOW(6), 'LIKE', 'DAILY', 'FEMALE', 1, 3, 2, 1),
+       (NOW(6), NOW(6), 'INTERVIEW', 'CHALLENGE', 'ALL', 1, 1, 30, 1),
+       (NOW(6), NOW(6), 'FIRST_DATE_EXAM', 'CHALLENGE', 'ALL', 1, 1, 15, 1)
