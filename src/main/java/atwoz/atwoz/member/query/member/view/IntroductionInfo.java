9838a0f279f03b2175241b6ba719d4c7df9086b9
hainho <hainho9704@gmail.com>
2025-10-24 14:30:33 +0900

    feat: 상대 프로필 조회 DTO에 소개, 매치 관련 enum 정보 스웨거에 노출

--- src/main/java/atwoz/atwoz/member/query/member/view/IntroductionInfo.java
+++ src/main/java/atwoz/atwoz/member/query/member/view/IntroductionInfo.java
@@ -1,6 +1,10 @@
 package atwoz.atwoz.member.query.member.view;
 
+import atwoz.atwoz.member.command.domain.introduction.IntroductionType;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record IntroductionInfo(
+    @Schema(implementation = IntroductionType.class)
     String introductionType
 ) {
 }
