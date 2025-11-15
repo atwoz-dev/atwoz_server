2ec7a719bbdc994de1ac0a69803e2783c30adae6
hainho <hainho9704@gmail.com>
2025-11-15 20:13:41 +0900

    fix: 테스트 메서드 명 오타 수정

--- src/test/java/atwoz/atwoz/member/query/member/AgeConverterTest.java
+++ src/test/java/atwoz/atwoz/member/query/member/AgeConverterTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AgeConverterTest {
     @Test
     @DisplayName("연도를 나이로 변환하고, 다시 나이를 연도로 변환하면 같은 연도가 나와야 한다.")
-    void toAgeAndBackThanSameYearOfBirth() {
+    void toAgeAndBackThenSameYearOfBirth() {
         // given
         Integer yearOfBirth = 1997;
 
