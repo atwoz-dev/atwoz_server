e58918ea6a661ebc6f817774e60c2e321fe8930b
공태현 <dev0taehyeon@gmail.com>
2025-11-16 19:24:05 +0900

    [Feat] : Implement Query Layer to select matches

---
+++ src/main/java/atwoz/atwoz/match/query/MatchQueryRepository.java
@@ -0,0 +1,118 @@
+package atwoz.atwoz.match.query;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static atwoz.atwoz.block.domain.QBlock.block;
+import static atwoz.atwoz.match.command.domain.match.QMatch.match;
+import static atwoz.atwoz.member.command.domain.member.QMember.member;
+import static atwoz.atwoz.member.command.domain.profileImage.QProfileImage.profileImage;
+
+@Repository
+@RequiredArgsConstructor
+public class MatchQueryRepository {
+    private static final int PAGE_SIZE = 13;
+    private final JPAQueryFactory queryFactory;
+
+    /**
+     * 자신이 요청한 매칭 내역.
+     */
+    public List<MatchView> findSentMatches(long requesterId, Long lastMatchId) {
+        Set<Long> blockedIds = getBlockedIds(requesterId);
+
+        return queryFactory
+            .select(new QMatchView(
+                match.id,
+                match.responderId,
+                match.responseMessage.value,
+                member.profile.nickname.value,
+                profileImage.imageUrl.value,
+                member.profile.region.city.stringValue(),
+                match.requestMessage.value,
+                match.status.stringValue(),
+                match.createdAt
+            ))
+            .from(match)
+            .where(
+                eqRequesterId(requesterId),
+                ltMatchId(lastMatchId),
+                responderIdNotIn(blockedIds)
+            )
+            .leftJoin(member).on(member.id.eq(match.responderId))
+            .leftJoin(profileImage).on(profileImage.memberId.eq(member.id).and(profileImage.isPrimary.eq(true)))
+            .orderBy(match.id.desc())
+            .limit(PAGE_SIZE)
+            .fetch();
+    }
+
+    public List<MatchView> findReceiveMatches(long responderId, Long lastMatchId) {
+        Set<Long> blockedIds = getBlockedIds(responderId);
+
+        return queryFactory
+            .select(new QMatchView(
+                match.id,
+                match.requesterId,
+                match.requestMessage.value,
+                member.profile.nickname.value,
+                profileImage.imageUrl.value,
+                member.profile.region.city.stringValue(),
+                match.responseMessage.value,
+                match.status.stringValue(),
+                match.createdAt
+            ))
+            .from(match)
+            .where(
+                eqResponderId(responderId),
+                ltMatchId(lastMatchId),
+                requesterIdNotIn(blockedIds)
+            )
+            .leftJoin(member).on(member.id.eq(match.requesterId))
+            .leftJoin(profileImage).on(profileImage.memberId.eq(member.id).and(profileImage.isPrimary.eq(true)))
+            .orderBy(match.id.desc())
+            .limit(PAGE_SIZE)
+            .fetch();
+    }
+
+
+    private Set<Long> getBlockedIds(long blockerId) {
+        return queryFactory
+            .select(block.blockedId)
+            .from(block)
+            .where(block.blockerId.eq(blockerId))
+            .fetch()
+            .stream()
+            .collect(Collectors.toSet());
+    }
+
+    private BooleanExpression eqRequesterId(long requesterId) {
+        return match.requesterId.eq(requesterId);
+    }
+
+    private BooleanExpression eqResponderId(long responderId) {
+        return match.responderId.eq(responderId);
+    }
+
+    private BooleanExpression requesterIdNotIn(Set<Long> ids) {
+        if (ids.isEmpty()) {
+            return null;
+        }
+        return match.requesterId.notIn(ids);
+    }
+
+    private BooleanExpression responderIdNotIn(Set<Long> ids) {
+        if (ids.isEmpty()) {
+            return null;
+        }
+        return match.responderId.notIn(ids);
+    }
+
+    private BooleanExpression ltMatchId(Long lastMatchId) {
+        return lastMatchId != null ? match.id.lt(lastMatchId) : null;
+    }
+}
---
+++ src/main/java/atwoz/atwoz/match/query/MatchQueryService.java
@@ -0,0 +1,26 @@
+package atwoz.atwoz.match.query;
+
+import atwoz.atwoz.match.presentation.dto.MatchViews;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MatchQueryService {
+    private static final int CLIENT_PAGE_SIZE = 12;
+    private final MatchQueryRepository matchQueryRepository;
+
+    public MatchViews getSentMatches(long requesterId, Long lastMatchId) {
+        List<MatchView> matches = matchQueryRepository.findSentMatches(requesterId, lastMatchId);
+        boolean hasMore = matches.size() > CLIENT_PAGE_SIZE;
+        return new MatchViews(matches, hasMore);
+    }
+
+    public MatchViews getReceivedMatches(long responderId, Long lastMatchId) {
+        List<MatchView> matches = matchQueryRepository.findReceiveMatches(responderId, lastMatchId);
+        boolean hasMore = matches.size() > CLIENT_PAGE_SIZE;
+        return new MatchViews(matches, hasMore);
+    }
+}
