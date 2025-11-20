ba5b07bb263c9354edf9fd654a4da32e4e989ffe
Ha In Ho <hainho9704@gmail.com>
2025-11-20 11:32:05 +0900

    fix: exception handler에서 order 누락된 케이스에 order 추가 (#372)

--- src/main/java/atwoz/atwoz/community/presentation/selfintroduction/SelfIntroductionExceptionHandler.java
+++ src/main/java/atwoz/atwoz/community/presentation/selfintroduction/SelfIntroductionExceptionHandler.java
@@ -5,12 +5,15 @@ import atwoz.atwoz.common.response.BaseResponse;
 import atwoz.atwoz.community.command.application.selfintroduction.exception.NotSelfIntroductionAuthorException;
 import atwoz.atwoz.community.command.application.selfintroduction.exception.SelfIntroductionNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SelfIntroductionExceptionHandler {
 
     @ExceptionHandler(SelfIntroductionNotFoundException.class)
--- src/main/java/atwoz/atwoz/member/presentation/introduction/MemberIntroductionExceptionHandler.java
+++ src/main/java/atwoz/atwoz/member/presentation/introduction/MemberIntroductionExceptionHandler.java
@@ -8,12 +8,15 @@ import atwoz.atwoz.member.command.application.introduction.exception.MemberIdeal
 import atwoz.atwoz.member.command.application.introduction.exception.MemberIntroductionAlreadyExistsException;
 import atwoz.atwoz.member.command.domain.introduction.exception.InvalidAgeRangeException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class MemberIntroductionExceptionHandler {
 
     @ExceptionHandler(MemberIdealNotFoundException.class)
