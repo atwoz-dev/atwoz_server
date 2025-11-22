4327861377e7402f1e2c2f212cf0d8b5d58f0b75
hainho <hainho9704@gmail.com>
2025-11-22 13:42:05 +0900

    fix: 활동중 상태가 아니거나, 차단하거나, 차단된 경우 프로필 교환 요청 예외 던지도록 수정

---
+++ src/main/java/atwoz/atwoz/community/command/application/profileexchange/exception/ProfileExchangeResponderNotActiveException.java
@@ -0,0 +1,7 @@
+package atwoz.atwoz.community.command.application.profileexchange.exception;
+
+public class ProfileExchangeResponderNotActiveException extends RuntimeException {
+    public ProfileExchangeResponderNotActiveException() {
+        super("프로필 교환 요청을 받은 사용자가 ACTIVE 상태가 아닙니다.");
+    }
+}
