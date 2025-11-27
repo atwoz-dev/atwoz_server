51b9295bc1065b92b4e3c7edf3429d1f942da214
hainho <hainho9704@gmail.com>
2025-11-27 09:21:07 +0900

    feat: time zone 설정 추가

--- src/main/java/atwoz/atwoz/AtwozApplication.java
+++ src/main/java/atwoz/atwoz/AtwozApplication.java
@@ -1,11 +1,14 @@
 package atwoz.atwoz;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -15,5 +18,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AtwozApplication {
     public static void main(String[] args) {
         SpringApplication.run(AtwozApplication.class, args);
+    }
+
+    @PostConstruct
+    public void started() {
+        // set timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
